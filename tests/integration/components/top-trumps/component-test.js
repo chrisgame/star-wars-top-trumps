a0bfca2bf6b487f397c6b7574565e28af5e49fc7
The Game <chris.game@robbiebobbins.com>
2018-04-13 17:31:58 +0100

    test scenarios for game

---
+++ tests/integration/components/top-trumps/component-test.js
@@ -0,0 +1,15 @@
+import { module, skip } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+
+module('Integration | Component | top-trumps', function(hooks) {
+  setupRenderingTest(hooks);
+
+  skip('when a game is started the deck is delt evenly between the players', async function() {});
+  skip('when the deck is delt and there is no previous winner the user must choose the attribute to be used', async function() {});
+  skip('when the deck is delt and there is a previous winner the previous winner must choose the attribute to be used', async function() {});
+  skip('when an attribute has been selected by the user the computer draws a card', async function() {});
+  skip('when an attribute has been selected by the computer the user draws a card', async function() {});
+  skip('when both cards are drawn the winner is chosen', async function() {});
+  skip('when a winner is chosen the game continues if players have any cards remaining in there hand', async function() {});
+  skip('the game ends when players have used all there cards', async function() {});
+});
