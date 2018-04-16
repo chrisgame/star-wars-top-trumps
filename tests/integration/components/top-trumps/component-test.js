e42c85869cb5990946dba0c02187981e022b80f6
The Game <chris.game@robbiebobbins.com>
2018-04-16 09:58:28 +0100

    fixup! test scenarios for game

--- tests/integration/components/top-trumps/component-test.js
+++ tests/integration/components/top-trumps/component-test.js
@@ -36,6 +36,6 @@ module('Integration | Component | top-trumps', function(hooks) {
   skip('when an attribute has been selected by the user the computer draws a card', async function() {});
   skip('when an attribute has been selected by the computer the user draws a card', async function() {});
   skip('when both cards are drawn the winner is chosen', async function() {});
-  skip('when a winner is chosen the game continues if players have any cards remaining in there hand', async function() {});
-  skip('the game ends when players have used all there cards', async function() {});
+  skip('when a winner is chosen the game continues if players have any cards remaining in their hand', async function() {});
+  skip('the game ends when players have used all their cards', async function() {});
 });
