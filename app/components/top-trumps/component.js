fb9f3459f68fa8b263f7267527a550f0c7083efc
The Game <chris.game@robbiebobbins.com>
2018-04-16 09:39:23 +0100

    the game is started and the deck is delt

--- app/components/top-trumps/component.js
+++ app/components/top-trumps/component.js
@@ -1,4 +1,57 @@
 import Component from '@ember/component';
+import { computed } from '@ember/object';
 
 export default Component.extend({
+  //Attrs
+  deck: null,
+
+  //State
+  started: false,
+
+  init() {
+    this._super(...arguments);
+    this.set('players', 2);
+  },
+
+  shuffledDeck: computed('deck', function() {
+    let deck = this.get('deck');
+
+    return deck.map(a => [Math.random(), a])
+      .sort((a, b) => a[0] - b[0])
+      .map(a => a[1]);
+  }),
+
+  hands: computed('shuffledDeck', function() {
+    let players = this.get('players');
+    let shuffledDeck = this.get('shuffledDeck');
+    let hands = [];
+    let splitOn = shuffledDeck.length / players;
+
+    hands[0] = shuffledDeck.slice(0, splitOn);
+    hands[1] = shuffledDeck.slice(splitOn);
+
+    return hands;
+  }),
+
+  turns: computed('hands', function() {
+    let hands = this.get('hands');
+
+    return hands[0].length;
+  }),
+
+  turnsText: computed('turns', function() {
+    let turns = this.get('turns');
+
+    if (turns === 1) {
+      return "1 turn remaining";
+    } else {
+      return `${turns} remaining`;
+    }
+  }),
+
+  actions: {
+    startGame() {
+      this.set('started', true);
+    }
+  }
 });
--- tests/integration/components/top-trumps/component-test.js
+++ tests/integration/components/top-trumps/component-test.js
@@ -1,10 +1,36 @@
-import { module, skip } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { render, click } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | top-trumps', function(hooks) {
   setupRenderingTest(hooks);
 
-  skip('when a game is started the deck is delt evenly between the players', async function() {});
+  hooks.beforeEach(function() {
+    this.twoCardDeck = [
+      {
+        "name": "Luke Skywalker",
+          "height": "172",
+          "mass": "77"
+      },
+      {
+        "name": "Captain Phasma",
+          "height": "199",
+          "mass": "70",
+      }
+    ];
+  }),
+
+  test('when a game is started the deck is delt evenly between the players', async function(assert) {
+    this.set('deck', this.twoCardDeck);
+    await render(hbs`{{top-trumps deck=deck}}`);
+
+    assert.dom('#turns').doesNotExist();
+
+    await click('#start');
+
+    assert.dom('#turns').hasText('1 turn remaining');
+  });
   skip('when the deck is delt and there is no previous winner the user must choose the attribute to be used', async function() {});
   skip('when the deck is delt and there is a previous winner the previous winner must choose the attribute to be used', async function() {});
   skip('when an attribute has been selected by the user the computer draws a card', async function() {});
